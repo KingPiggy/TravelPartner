37778505a0ddf7bb5383368e54c40b0d76a37b97
KingGodLSH <best2204@naver.com>
2018-08-06 16:27:29 +0900

    2018.8.6 Hoon) Create User Settings Activity

--- app/src/main/java/kr/ac/shinhan/travelpartner/MainActivity.java
+++ app/src/main/java/kr/ac/shinhan/travelpartner/MainActivity.java
@@ -1,13 +1,53 @@
 package kr.ac.shinhan.travelpartner;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PREFNAME = "Preferences";
+    static final int USERSETTINGS = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isFirstTime();
+
+
+
+
+    }
+
+    public void isFirstTime(){
+        SharedPreferences settings = getSharedPreferences(PREFNAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        if(settings.getBoolean("isFirstTime", true)){
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+
+            Intent intent = new Intent(getApplicationContext(), UserPrefActivity.class);
+            startActivityForResult(intent, USERSETTINGS);
+        }
+        else{
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case USERSETTINGS:
+                if(resultCode == Activity.RESULT_OK){
+
+                }
+                break;
+        }
+
     }
 }
