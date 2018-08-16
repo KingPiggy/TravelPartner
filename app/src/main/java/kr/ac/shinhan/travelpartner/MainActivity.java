a3f02bfd3c695af43ed3638d065171be9c6e9ed6
Bae <qpyu66@gmail.com>
2018-08-16 17:44:08 +0900

    여행지 탭 다시해주세요

--- app/src/main/java/kr/ac/shinhan/travelpartner/MainActivity.java
+++ app/src/main/java/kr/ac/shinhan/travelpartner/MainActivity.java
@@ -5,19 +5,53 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.TabHost;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String PREFNAME = "Preferences";
     public static final int USERSETTINGS = 10000;
     public static final int PERMISSION_INTERNET = 100;
     public static final int PERMISSON_ACCESS_FINE_LOCATION = 200;
+    TabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         isFirstTime();
+
+        tabHost = (TabHost)findViewById(R.id.tapHost);
+
+        tabHost.setup();
+
+
+//        tabHost = getTabHost();
+
+
+//        TabHost.TabSpec tabSpec = tabHost.newTabSpec("tab_test1");
+//        tabSpec.setIndicator("Map");
+//        Context ctx = this.getApplicationContext();
+//        Intent i = new Intent(ctx, MapsActivity.class);
+//        tabSpec.setContent(i);
+//        tabHost.addTab(tabSpec);
+//        tabHost.addTab(tabHost.newTabSpec("Map2").setIndicator("맵").setContent(R.id.extext2));
+//        tabHost.setCurrentTab(0);
+
+
+        tabHost.addTab(tabHost.newTabSpec("Home").setContent(R.id.tab1).setIndicator("홈타이틀"));
+
+        Intent intent;
+        intent = new Intent().setClass(this,MapsActivity.class);
+        tabHost.addTab(tabHost.newTabSpec("Travel")
+                .setIndicator("여행지")
+                .setContent(intent));
+
+
+        tabHost.addTab(tabHost.newTabSpec("MyPage")
+                .setIndicator("마이페이지")
+                .setContent(R.id.tab3));
 
     }
 
@@ -48,11 +82,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void move(View v){
-        switch (v.getId()) {
-            case R.id.btn_main_map:
-                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-                startActivity(intent);
-        }
-    }
+//    public void move(View v){
+//        switch (v.getId()) {
+//            case R.id.btn_main_map:
+//                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+//                startActivity(intent);
+//        }
+//    }
 }
